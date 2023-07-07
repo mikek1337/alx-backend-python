0cb968681631fa6f8633a7ecc257d95cf55b5e9b
mikek1337 <mikekegnlyew@gmail.com>
2023-07-07 09:49:00 +0300

    a function that returns a muitiplier function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns function that multiplies float by multiplier"""
+    return lambda x: x * multiplier
