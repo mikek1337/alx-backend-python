bb115f00a767439a67017350a9dff0c889e26945
mikek1337 <mikekegnlyew@gmail.com>
2023-07-31 19:38:20 +0300

    integration test for githuborg

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -43,3 +43,61 @@ class TestGithubOrgClient(unittest.TestCase):
             test_class = GithubOrgClient("test")
             self.assertEqual(test_class.public_repos(), ["google", "abc"])
             mock_get_json.assert_called_once_with("test")
+
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'my_license', False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """test_has_license method"""
+        self.assertEqual(GithubOrgClient.has_license(repo, license_key),
+                         expected)
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    [
+        (
+            {"repos_url": "http://test.com"},
+            [{"name": "google"}, {"name": "abc"}],
+            ["google", "abc"],
+            ["google"]
+        ),
+        (
+            {"repos_url": "http://test.com"},
+            [{"name": "google"}, {"name": "abc", "license": {"key": "apache2"}}],
+            ["google", "abc"],
+            ["abc"]
+        )
+    ]
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """TestIntegrationGithubOrgClient class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """setUpClass method"""
+        config = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **config)
+
+        cls.mock_get = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """tearDownClass method"""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """test_public_repos method"""
+        test_class = GithubOrgClient("test")
+        self.assertEqual(test_class.org, self.org_payload)
+        self.assertEqual(test_class.repos_payload, self.repos_payload)
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos(
+            "apache2"), self.apache2_repos)
+        self.mock_get.assert_called()
